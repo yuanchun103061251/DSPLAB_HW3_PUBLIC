436787548a52bf0066347e93dbd85cdf0d5d5429
TingAnChien <tingan0206@gmail.com>
2016-09-07 01:07:16 +0800

    Update findOptSeam.m

--- code/findOptSeam.m
+++ code/findOptSeam.m
@@ -1,48 +1,54 @@
 function [optSeamMask, seamEnergy] = findOptSeam(energy)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% following paper by Avidan and Shamir `07
-% finds optimal seam by the given 
-% returns mask with 0 mean a pixel is in the seam
+% Following paper by Avidan and Shamir `07
+% Finds optimal seam by the given energy of an image
+% Returns mask with 0 mean a pixel is in the seam
 % You only need to implement vertical seam. For
 % horizontal case, just using the same function by 
 % giving energy for the transpose image I'.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-    % find M for vertical seams
-    % for vertical - use I`
-    M = padarray(energy, [0 1], realmax('double')); % to avoid handling border elements
+    % Find M for vertical seams
+    % Add one element of padding in vertical dimension 
+    % to avoid handling border elements
+    M = padarray(energy, [0 1], realmax('double'));
 
     sz = size(M);
-    for i = 2 : sz(1)
-        for j = 2 : (sz(2) - 1)
-            neighbors = [M(i - 1, j - 1) M(i - 1, j) M(i - 1, j + 1)];
-            M(i, j) = M(i, j) + min(neighbors);
-        end
-    end
+    
+    % For all rows starting from second row, fill in the minimum 
+    % energy for all possible seam for each (i,j) in M, which
+    % M[i, j] = e[i, j] + min(M[i - 1, j], M[i, j], M[i + 1, j]).     
+    
+    %%%%%%%%%%%%%%%%%%
+    % YOUR CODE HERE:
+    %%%%%%%%%%%%%%%%%%
+    
+    %%%%%%%%%%%%%%%%%%
+    % END OF YOUR CODE
+    %%%%%%%%%%%%%%%%%%
+    
 
-    % find the min element in the last raw
-    [val, indJ] = min(M(sz(1), :));
+    % Find the minimum element in the last raw of M
+    [val, idx] = min(M(sz(1), :));
     seamEnergy = val;
     
-    %optSeam = zeros(sz(1), 1, 'int32');
+    % Initial for optimal seam mask
     optSeamMask = zeros(size(energy), 'uint8');
     
-    %indJ = indJ - 1; % because of padding on 1 element from left
- 
-    %go backward and save (i, j)
-    for i = sz(1) : -1 : 2
-        %optSeam(i) = indJ - 1;
-        optSeamMask(i, indJ - 1) = 1; % -1 because of padding on 1 element from left
-        neighbors = [M(i - 1, indJ - 1) M(i - 1, indJ) M(i - 1, indJ + 1)];
-        [val, indIncr] = min(neighbors);
-        
-        seamEnergy = seamEnergy + val;
-        
-        indJ = indJ + (indIncr - 2); % (x - 2): [1,2]->[-1,1]]
-    end
-    %optSeam(1) = indJ; % to avoid if in loop becuase matlab is slow as hell
-
-    optSeamMask(1, indJ - 1) = 1; % -1 because of padding on 1 element from left
-    optSeamMask = ~optSeamMask;
+    % Traverse back the path of seam with minimum energy
+    % and update optimal seam mask, which (i,j) value of 
+    % a seam should be set to 1 here
+    % (Aware the size of mask and the M is different)
+    
+    %%%%%%%%%%%%%%%%%%
+    % YOUR CODE HERE:
+    %%%%%%%%%%%%%%%%%%
+    
+    %%%%%%%%%%%%%%%%%%
+    % END OF YOUR CODE
+    %%%%%%%%%%%%%%%%%%
+    
+    % convert the mask to logical
+    optSeamMask = ~optSeamMask; 
     
 end
